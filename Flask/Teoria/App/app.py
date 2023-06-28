c2085d4db0850bb412f2c4403ae54b99ed45a7cf
Carlos Hidalgo <hidalgogonzalez@hotmail.com>
2023-06-28 18:45:52 +0200

    a
    a

--- Flask/Teoria/App/app.py
+++ Flask/Teoria/App/app.py
@@ -1,40 +1,13 @@
 from flask import Flask
+app = Flask(__name__)
 
-<<<<<<< HEAD
-p = Flask(__name__, instance_relative_config=True)
-app.config.from_object("config")
-app.config.from_pyfile("config.py")
-
-@app.route('/')
-def index():
-    return "<h1>Hello World!</h1>"
-
-#app.add_url_rule("/","index",index)
-
-@app.route("/user/<name>")
-def user(name):
-    return "<h1>Hello, {}!</h1>".format(name)
-
-=======
-
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object("config")
-app.config.from_pyfile("config.py")
-
-
-@app.route('/')
-def index():
-    return "<h1>Hello World !</h1>"
-
-# app.add_url_rule("/","index",index)
-
-@app.route("/user/<name>")
-def user(name: str):
-    return "<h1>Hello , {}!</h1>".format(name)
-
-@app.route("/user1/<name>")
-def user1(name):
-    return "<h1>Bye, {}!</h1>".format(name)
->>>>>>> upstream/main
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
